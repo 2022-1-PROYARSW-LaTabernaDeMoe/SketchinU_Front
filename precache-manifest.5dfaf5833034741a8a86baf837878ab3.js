67f35c4b80a0125ed7337c6dafd7ca11bdc2db87
Jandres1420 <juan.pico-m@mail.escuelaing.edu.co>
2022-03-29 15:47:40 -0500

    Updates

---
+++ precache-manifest.5dfaf5833034741a8a86baf837878ab3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f94cafdd7775877d5e2d9cba6b6bccc9",
+    "url": "/SketchingU_Front/index.html"
+  },
+  {
+    "revision": "9ebf20563e7d7a18d444",
+    "url": "/SketchingU_Front/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9ebf20563e7d7a18d444",
+    "url": "/SketchingU_Front/static/js/2.b1ed7d04.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/SketchingU_Front/static/js/2.b1ed7d04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dca0e0ec7ebbb6faf7fb",
+    "url": "/SketchingU_Front/static/js/main.a314d886.chunk.js"
+  },
+  {
+    "revision": "daaf229b6a9ee146dbac",
+    "url": "/SketchingU_Front/static/js/runtime-main.1c5973f6.js"
+  }
+]);
